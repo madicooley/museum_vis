6c3ab65db89c6731d70b0da534ab8186d68f3fdc
Cole Polychronis <colepolychronis@Coles-MacBook-Pro-4.local>
2019-10-30 13:40:20 -0600

    created function to isolate museum name from file name

--- data/data-processor.py
+++ data/data-processor.py
@@ -7,10 +7,31 @@ Authors: Derya Akbaba, Madi Cooley, and Cole Polychronis
 # import relevant modules
 import os
 import glob
+import csv
 
 def main():
     for file in sorted(glob.glob("./raw-data/*-*.csv")):
-        print(file)
+        print('Museum: ' + extractMuseum(file))
+        # for i, row in enumerate(csv.reader(open(file, "rU"))):
+        #     # print(row)
+
+'''
+Function to take a file name and isolate the name of the museum
+
+Parameters:
+filename: name of the dataset file
+
+Returns:
+string: name of the museum
+'''
+def extractMuseum(fileName):
+    # find last instance of '/' character
+    slash_index = fileName.rfind('/') + 1
+    # find last instance of '.csv' substring
+    fileEXT_index = fileName.rfind('.csv')
+
+    return fileName[slash_index:fileEXT_index]
+
 
 if __name__ == "__main__":
     main()
