3852ad16916ae602b1a29e4466d49a1c93f39a8a
derya <dakbaba@locus.co>
2019-10-31 15:16:23 -0600

    Some styling for text and map. Added script and map js files

---
+++ js/script.js
@@ -0,0 +1,36 @@
+loadData().then(data => {
+
+  // Creates the view objects
+  const worldMap = new Map(data);
+
+  // here we load the map data
+  d3.json('data/world.json').then(mapData => {
+
+    worldMap.drawMap(mapData);
+
+  });
+
+  async function loadFile(file) {
+    let data = await d3.csv(file).then(d => {
+      let mapped = d.map(g => {
+        for (let key in g) {
+          let numKey = +key;
+          if (numKey) {
+            g[key] = +g[key];
+          }
+        }
+        return g;
+      });
+      return mapped;
+    });
+    return data;
+  }
+
+  async function loadData() { //this is probably overkill
+    let geoData = await loadFile('data/temp_data.csv');
+
+    return {
+      'geoData': geoData
+    };
+  }
+})
