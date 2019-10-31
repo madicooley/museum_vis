9ea205bb3fb6dfab07fa3f168005ce2f8c3b3ad7
Cole Polychronis <colepolychronis@Coles-MacBook-Pro-4.local>
2019-10-31 09:23:42 -0600

    created init file with column numbers of the relevant attributes. created a helper function in data-processor to print first few rows of datasets

--- data/data-processor.py
+++ data/data-processor.py
@@ -8,21 +8,13 @@ Authors: Derya Akbaba, Madi Cooley, and Cole Polychronis
 import os
 import glob
 import csv
+from data_init import column_args
 
 def main():
-    for file in sorted(glob.glob("./raw-data/*-*.csv")):
-        print('Museum: ' + extractMuseum(file))
-        # for i, row in enumerate(csv.reader(open(file, "rU"))):
-        #     # print(row)
+    printHead(5)
 
 '''
 Function to take a file name and isolate the name of the museum
-
-Parameters:
-filename: name of the dataset file
-
-Returns:
-string: name of the museum
 '''
 def extractMuseum(fileName):
     # find last instance of '/' character
@@ -31,6 +23,22 @@ def extractMuseum(fileName):
     fileEXT_index = fileName.rfind('.csv')
 
     return fileName[slash_index:fileEXT_index]
+
+'''
+A helper function that prints the first number of specfied rows from each CSV
+'''
+def printHead(rowNum):
+    for file in sorted(glob.glob("./raw-data/*-*.csv")):
+        museum_name = extractMuseum(file)
+        museum_delimiter = '|' if museum_name == 'canada-science-and-technology-museums' else ','
+        print(museum_name)
+        for i, row in enumerate(csv.reader(open(file, 'rU'), delimiter = museum_delimiter)):
+            if i < rowNum:
+                possbile_row = [row[j] for j in column_args[museum_name]]
+                if '' in possbile_row:
+                    print('missing vals')
+                else:
+                    print(possbile_row)
 
 
 if __name__ == "__main__":
