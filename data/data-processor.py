4aee806e32b602b1ac15b31e3f1314af408aa0a7
Cole Polychronis <colepolychronis@Coles-MacBook-Pro-4.local>
2019-11-01 11:14:00 -0600

    deletes cleaned-data.csv on run before creating it. Full processing for canada science and technology museums

--- data/data-processor.py
+++ data/data-processor.py
@@ -5,13 +5,28 @@ Authors: Derya Akbaba, Madi Cooley, and Cole Polychronis
 '''
 
 # import relevant modules
+import sys
 import os
 import glob
 import csv
 from data_init import column_args
+import pycountry_convert as pc
 
 def main():
-    printHead(5)
+    csv.field_size_limit(sys.maxsize)
+
+    file_dict = {}
+
+    # read in all csv files and store them in a dictionary
+    for file in sorted(glob.glob("./raw-data/*-*.csv")):
+        museum_name = extractMuseum(file)
+        file_dict[museum_name] = file
+
+    os.remove("cleaned-data.csv")
+    out = csv.writer(open("cleaned-data.csv", "w"))
+
+    processCanada(file_dict['canada-science-and-technology-museums'], out)
+    # printHead(5)
 
 '''
 Function to take a file name and isolate the name of the museum
@@ -32,7 +47,7 @@ def printHead(rowNum):
         museum_name = extractMuseum(file)
         museum_delimiter = '|' if museum_name == 'canada-science-and-technology-museums' else ','
         print(museum_name)
-        for i, row in enumerate(csv.reader(open(file, 'rU'), delimiter = museum_delimiter)):
+        for i, row in enumerate(csv.reader(open(file, 'rU'), delimiter = museum_delimitere)):
             if i < rowNum:
                 possbile_row = [row[j] for j in column_args[museum_name]]
                 if '' in possbile_row:
@@ -40,6 +55,40 @@ def printHead(rowNum):
                 else:
                     print(possbile_row)
 
+'''
+Function to process the canada science and technology museums dataset
+'''
+def processCanada(file, out):
+    museum_name = extractMuseum(file)
+    reader = csv.reader(open(file, 'rU'), delimiter = '|')
+    
+    for i, row in enumerate(reader):
+        if i > 0: # skips header row of csv file
+            try: 
+                possbile_row = [row[j] for j in column_args[museum_name]]
+            except:
+                # row isnt populated enough
+                pass
+            
+            if '' not in possbile_row and possbile_row[1] != 'Unknown': # check that row has all necessary data
+                # extract acquisition date
+                acq_date = possbile_row[2]
+                possbile_row[2] = acq_date[0: acq_date.find('.')]
+
+                # append continent
+                try: 
+                    country_code  = pc.country_name_to_country_alpha2(possbile_row[1], cn_name_format="default")
+                    possbile_row.append(pc.country_alpha2_to_continent_code(country_code))
+                except:
+                    # country isnt listed 
+                    pass
+
+                try:
+                    possbile_row[5]
+                    out.writerow(possbile_row)
+                except:
+                    # continent no listed for country
+                    pass
 
 if __name__ == "__main__":
     main()
