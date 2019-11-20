4a7f8d7cff89c1528a8cd051a56073ec77d225f8
Cole Polychronis <colepolychronis@Coles-MacBook-Pro-4.local>
2019-11-20 14:34:58 -0700

    able to create an aggregated KDE plot. Now need to create one for eaach museum and superimpose them

--- js/kdePlot.js
+++ js/kdePlot.js
@@ -19,8 +19,12 @@
 
         // initialize svg and group that we will append plot elements to
         let svg = d3.select('#kde-plot');
-        svg.attr('height', 600)
-            .attr('width', 1000);
+        let margins = {'left': 35, 'right': 35, 'top': 25, 'bottom': 35}
+        let height = 600;
+        let width = 1000;
+
+        svg.attr('height', height + margins.top + margins.bottom)
+            .attr('width', width + margins.left + margins.right);
         let plotGroup = svg.append('g')
             .attr('id', 'plot-group');
 
@@ -48,8 +52,74 @@
         }
 
         // create axis for plot
+        let xScale = d3.scaleLinear()
+            .domain(extrema).nice()
+            .range([0,width])
+        plotGroup.append('g')
+            .attr('id', 'x-axis')
+            .attr('transform', `translate(${margins.left},${height-margins.bottom})`)
+            .call(d3.axisBottom(xScale));
+
+        // create bins like what would be in a histogram to use for KDE
+        let thresholds = xScale.ticks(40) // create 40 bin limits
+        let bins = d3.histogram()
+            .domain(xScale.domain())
+            .thresholds(thresholds)(this.plotData.map((d) => d.acquisition_date)) // change this from acquisition date
+
+        let yScale = d3.scaleLinear() // need to fix this scale
+            .domain([0, d3.max(bins, d => d.length) / this.plotData.length])
+            .range([height - margins.top - margins.bottom, 0]);
+        plotGroup.append('g')
+            .attr('id', 'y-axis')
+            .attr('transform', `translate(${margins.left},${margins.top})`)
+            .call(d3.axisLeft(yScale).ticks(null, "%"))
+            .call(g => g.select(".domain").remove())
+
+        // Compute kernel density estimation
+        let density = this.kde(this.epanechnikov(7), thresholds, this.plotData.map((d) => d.acquisition_date)) // initial bandwidth = 7
+
+        // Plot bar charts for context
+        plotGroup.append("g")
+            .attr("fill", "#bbb")
+            .selectAll("rect")
+            .data(bins)
+            .join("rect")
+            .attr("x", d => xScale(d.x0) + 1)
+            .attr("y", d => yScale(d.length / this.plotData.length))
+            .attr("width", d => xScale(d.x1) - xScale(d.x0))
+            .attr("height", d => yScale(0) - yScale(d.length / this.plotData.length))
+            .attr('transform', `translate(${margins.left},${margins.top})`);
+
+        // Plot the area
+        let line = d3.line()
+            .curve(d3.curveBasis)
+            .x(d => xScale(d[0]))
+            .y(d => yScale(d[1]))
+        plotGroup.append("path")
+            .datum(density)
+            .attr('fill', 'none')
+            .attr('stroke', '#000')
+            .attr('stroke-width', 1.5)
+            .attr('stroke-linejoin', 'round')
+            .attr('d', line)
+            .attr('transform', `translate(${margins.left},${margins.top})`);
+
+
+
+
+
+
+
 
         // this.processData(this.data, this.vizCoord.activeYearOpt)
+    }
+
+    // Function to compute density
+    kde(kernel, thresholds, data) {
+        return thresholds.map(t => [t, d3.mean(data, d => kernel(t - d))]);
+    }
+    epanechnikov(bandwidth) {
+        return x => Math.abs(x /= bandwidth) <= 1 ? 0.75 * (1 - x * x) / bandwidth : 0;
     }
 
     /**
